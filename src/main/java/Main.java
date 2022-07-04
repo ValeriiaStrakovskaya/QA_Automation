83e61f3f37af6019d65384d0bb73af0a1dcefff9
ValeriiaStrakovsky <37975295+ValeriiaStrakovskaya@users.noreply.github.com>
2022-07-04 20:12:21 +0300

    new

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 5;
+        if (a == 5) {
+            switch (a) {
+                case 5:
+                    system.out.println(a);
+            }
+        }
+    }
+}
